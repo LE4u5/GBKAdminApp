e68b1f1916e4b1c71ee182f95a346bfa5a3fdf87
LE4u5 <tdav06@yahoo.com>
2020-06-20 00:50:43 -0400

    Added function to validate user login to LoginScreen.js

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -5,17 +5,29 @@ import { Input, Icon, Button } from 'react-native-elements';
 export default function LoginScreen(props) {
 
     const [state, setState] = useState({
-        loginValue: false
+        loginValue: false,
+        un: '',
+        pass: ''
     })
+
+    const validateUser = (user,pass) => {
+
+        const valid = ((user=='admin') && (pass === 'admin')) ? true : false;
+        if(valid)
+            setState({ ...state, loginValue: true });
+        else
+            setState({ ...state, loginValue: false });
+    }
+
     const TabBody = props.children
     const loginBody = <View style={styles.loginStyle}>
         <Image style={{ resizeMode: "contain", width: "90%", }} source={require('../assets/logo.png')} />
-        <Input label='Username' inputStyle={{ color: '#fff' }} labelStyle={{ color: '#000', marginBottom: 10 }} inputContainerStyle={styles.inputStyle} ></Input>
-        <Input label='Password' secureTextEntry={true} inputStyle={{ color: '#fff' }} labelStyle={{ color: '#000', marginBottom: 10 }} inputContainerStyle={styles.inputStyle}></Input>
+        <Input label='Username' inputStyle={{ color: '#fff' }} labelStyle={{ color: '#000', marginBottom: 10 }} inputContainerStyle={styles.inputStyle} value={state.un} onChangeText={text => setState({...state, un: text})} ></Input>
+        <Input label='Password' secureTextEntry={true} inputStyle={{ color: '#fff' }} labelStyle={{ color: '#000', marginBottom: 10 }} inputContainerStyle={styles.inputStyle} value={state.pass} onChangeText={text => setState({...state, pass: text})} ></Input>
         <Button
             raised title='Sign In'
             buttonStyle={{ backgroundColor: 'black', width: 150, borderRadius: 25 }}
-            onPress={() => toggleLogin()}
+            onPress={() => validateUser(state.un,state.pass)}
         />
     </View>
 
